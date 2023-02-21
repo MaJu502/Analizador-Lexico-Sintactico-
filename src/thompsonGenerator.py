7cfabfe928a5f7ff17f220d7e6e1f3f26d8f5f6f
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-02-20 18:13:15 -0600

    Update thompsonGenerator.py

---
+++ src/thompsonGenerator.py
@@ -0,0 +1,9 @@
+# Universidad del Valle de Guatemala
+# Marco Jurado 20308
+#
+# thompsonGenerator.py
+# construir un automata finito no deterministico (AFN) desde una 
+# expresión regular con base a el agoritmo de McNaughton-Yamada-Thompson
+
+def afnConstructor(exp):
+    pass
